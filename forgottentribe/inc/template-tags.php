2c90b44046a15c22715398187c994cd03256e649
Gitkohn <frankmur1983@hotmail.com>
2013-10-11 21:27:23 +0200

    added the theme

---
+++ forgottentribe/inc/template-tags.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Custom template tags for this theme.
+ *
+ * Eventually, some of the functionality here could be replaced by core features
+ *
+ * @package Forgotten Tribe
+ * @since Forgotten Tribe 1.0
+ */
