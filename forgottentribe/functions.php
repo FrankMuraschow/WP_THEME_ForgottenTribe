f7446c6557a873dd57319197ee99bd89ba309709
Gitkohn <frankmur1983@hotmail.com>
2013-10-14 22:23:26 +0200

    Completed Part 11
    http://themeshaper.com/2012/11/04/the-wordpress-theme-comments-template/

--- forgottentribe/functions.php
+++ forgottentribe/functions.php
@@ -63,3 +63,21 @@ function forgottentribe_setup() {
 }
 endif; // forgottentribe_setup
 add_action( 'after_setup_theme', 'forgottentribe_setup' );
+
+/**
+ * Enqueue scripts and styles
+ */
+function forgottentribe_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+ 
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+ 
+    wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+ 
+    if ( is_singular() && wp_attachment_is_image() ) {
+        wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'forgottentribe_scripts' );
